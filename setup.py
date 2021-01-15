a7ccbecd81220e8e81a9b29f0731cde75b9ef4bd
RASM01 <rafaelangelsalazar@gmail.com>
2021-01-15 01:24:16 -0400

    Instalanda

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
    name='bt_ccxt_store',
    version='1.0',
    description='A fork of Ed Bartosh\'s CCXT Store Work with some additions',
-   url='https://github.com/Dave-Vallance/bt-ccxt-store',
+   url='https://github.com/RASM01/bt-ccxt-store',
    author='Dave Vallance',
    author_email='dave@backtest-rookies.com',
    license='MIT',
